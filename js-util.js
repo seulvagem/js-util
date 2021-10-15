196775d23346c7682a0406d51667f254731a5ebd
selvagem <felipe_ferraz_dias_lab2dev@whirlpool.com>
2021-10-15 18:30:46 -0300

    init commit

---
+++ js-util.js
@@ -0,0 +1,31 @@
+const get = (key, obj) => {
+    return obj && obj[key]
+}
+
+const getIn = ([key, ...nextKeys], obj) => {
+    if (obj === undefined) {
+        return undefined
+    }
+
+    const value = get(key, obj)
+    if (nextKeys.length) {
+        return getIn(nextKeys, value)
+    } else {
+        return value
+    }
+}
+
+const select = (keyNames, obj) => {
+
+    const selectedEntries = keyNames.map((keyInput) => {
+        const [key, nKey] = (typeof(keyInput) === "string") ? [keyInput, keyInput] : keyInput
+        
+        const keys = (typeof(key) === "string") ? [key] : key
+
+        return [nKey, getIn(keys, obj)]
+    })
+
+    return Object.fromEntries(selectedEntries)
+}
+
+module.exports = {get, getIn, select}
